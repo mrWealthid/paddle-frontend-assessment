37d66d697e5e9e40b270ecf46f1ab7f00e541244
Mr-Wealth <81032094+mrWealthid@users.noreply.github.com>
2023-05-24 11:04:03 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
-import PageLinks from './components/PageLinks';
-import GitProfiles from './gitComponents/GitProfiles';
+// import PageLinks from './components/PageLinks';
+// import GitProfiles from './gitComponents/GitProfiles';
 
 function App() {
   return (
